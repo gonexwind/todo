2c454ea58b8e025e0fe6c049cdabd494753d306d
gonexwind <fikky.ardi15@gmail.com>
2022-08-30 08:17:17 +0700

    implement update

--- app/src/main/java/com/gonexwind/todo/ui/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/gonexwind/todo/ui/viewmodel/SharedViewModel.kt
@@ -68,10 +68,23 @@ class SharedViewModel @Inject constructor(
         }
     }
 
+    private fun updateTask() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val toDoTask = ToDoTask(
+                id = id.value,
+                title = title.value,
+                description = description.value,
+                priority = priority.value
+            )
+
+            repository.updateTask(toDoTask)
+        }
+    }
+
     fun handleDatabaseAction(action: Action) {
         when (action) {
             Action.ADD -> addTask()
-            Action.UPDATE -> {}
+            Action.UPDATE -> updateTask()
             Action.DELETE -> {}
             Action.DELETE_ALL -> {}
             Action.UNDO -> {}
