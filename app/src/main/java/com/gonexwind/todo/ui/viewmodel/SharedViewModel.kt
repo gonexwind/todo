268484921264cf716d5f8621b6dec47c883d7dcf
gonexwind <fikky.ardi15@gmail.com>
2022-08-30 08:27:24 +0700

    implement delete

--- app/src/main/java/com/gonexwind/todo/ui/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/gonexwind/todo/ui/viewmodel/SharedViewModel.kt
@@ -76,8 +76,19 @@ class SharedViewModel @Inject constructor(
                 description = description.value,
                 priority = priority.value
             )
-
             repository.updateTask(toDoTask)
+        }
+    }
+
+    private fun deleteTask() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val toDoTask = ToDoTask(
+                id = id.value,
+                title = title.value,
+                description = description.value,
+                priority = priority.value
+            )
+            repository.deleteTask(toDoTask)
         }
     }
 
@@ -85,7 +96,7 @@ class SharedViewModel @Inject constructor(
         when (action) {
             Action.ADD -> addTask()
             Action.UPDATE -> updateTask()
-            Action.DELETE -> {}
+            Action.DELETE -> deleteTask()
             Action.DELETE_ALL -> {}
             Action.UNDO -> {}
             Action.NO_ACTION -> {}
