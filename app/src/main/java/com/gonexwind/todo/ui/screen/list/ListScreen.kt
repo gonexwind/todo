760bd7318e846a350c3f849f599a9ebf3c3bf539
gonexwind <fikky.ardi15@gmail.com>
2022-08-30 08:10:04 +0700

    add snackbar to action

--- app/src/main/java/com/gonexwind/todo/ui/screen/list/ListScreen.kt
+++ app/src/main/java/com/gonexwind/todo/ui/screen/list/ListScreen.kt
@@ -2,18 +2,14 @@ package com.gonexwind.todo.ui.screen.list
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.res.stringResource
 import com.gonexwind.todo.R
 import com.gonexwind.todo.ui.viewmodel.SharedViewModel
+import com.gonexwind.todo.util.Action
 import com.gonexwind.todo.util.SearchAppBarState
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,10 +23,17 @@ fun ListScreen(
     val allTasks by sharedViewModel.allTasks.collectAsState()
     val searchAppBarState: SearchAppBarState by sharedViewModel.searchAppBarState
     val searchTextState: String by sharedViewModel.searchTextState
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    sharedViewModel.handleDatabaseAction(action)
+    DisplaySnackBar(
+        snackbarHostState,
+        { sharedViewModel.handleDatabaseAction(action) },
+        sharedViewModel.title.value,
+        action
+    )
 
     Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
         topBar = { ListAppBar(sharedViewModel, searchAppBarState, searchTextState) },
         content = { ListContent(allTasks, navigateToTaskScreen) },
         floatingActionButton = {
@@ -39,4 +42,23 @@ fun ListScreen(
             }
         }
     )
+}
+
+@Composable
+fun DisplaySnackBar(
+    snackbarHostState: SnackbarHostState,
+    handleDatabaseActions: () -> Unit,
+    taskTitle: String,
+    action: Action,
+) {
+    handleDatabaseActions()
+
+    val scope = rememberCoroutineScope()
+    LaunchedEffect(key1 = action) {
+        if (action != Action.NO_ACTION) {
+            scope.launch {
+                snackbarHostState.showSnackbar("${action.name} : $taskTitle")
+            }
+        }
+    }
 }
